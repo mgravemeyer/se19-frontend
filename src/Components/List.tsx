a576dd1dcecff46caaeb2eefb54db5142ca73a92
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-26 20:46:19 +0200

    added empty list

---
+++ src/Components/List.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface ListProps {
+    title: string;
+}
+
+export const List: React.FC<ListProps> = (title) => {
+    return (
+        <>
+            
+        </>
+    )
+}
