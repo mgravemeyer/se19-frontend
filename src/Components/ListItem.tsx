c70defaef52be0fd04c64fe6e7282fb825e62af6
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-26 21:30:26 +0200

    added ListItem component

---
+++ src/Components/ListItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface ListItemProps {
+
+}
+
+const ListItem: React.FC<ListItemProps> = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default ListItem;
