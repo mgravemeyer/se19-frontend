254e2f01285e0799ba7522f3ee111e5edf1aa6a3
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-27 09:09:27 +0200

    added form

--- src/Components/AddItem.tsx
+++ src/Components/AddItem.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import {useState} from 'react';
 
 export interface AddButtonProps {
     addItem: Function
 }
 
 const AddItem: React.FC<AddButtonProps> = ({addItem}) => {
-    return (
-        <div className="AddItem">
-            <button onClick={() => addItem()}>
 
-            </button>
-        </div>
+    const [inputText, setInputText] = useState('')
+
+    const onSubmit = (e: any) => {
+        e.preventDefault()
+        if(!inputText) {
+            alert('Please add a text')
+            return
+        }
+        addItem()
+        setInputText('')
+        return
+    }
+
+    return (
+        <form className='add-form' onSubmit={onSubmit}>
+            <input type='text' placeholder='Add Task' value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+            <input type='submit' value='Save Task'/>
+        </form>
+        // <button onClick={() => addItem()}></button>
     )
 }
 
